18f66b13b1599ac464c02b90f0e3e442b5ba86d4
mmccaskill <39064581+mmccaskill@users.noreply.github.com>
2018-05-08 12:17:40 -0500

    Add files via upload

--- media.py
+++ media.py
@@ -1,17 +1,44 @@
 import webbrowser
 
+
 class Movie():
+    """Class that describes the attributes of movies
 
-# this defines the movie title, storyline, poster and youtube trailer
+    Note:
+        Do not include the 'udacity' parameter in the "Args" section
 
-    def __init__(udacity, movie_title, movie_storyline, poster_image, trailer_youtube):
+    Args:
+        movie_title(str): Movie's title
+        movie_storyline(str): Movie's storyline
+        poster_image(str): Movie's poster image
+        trailer_youtube(str): Movie's trailer via youtube
+
+    Attributes:
+        movie_title(str): Movie's title
+        movie_storyline(str): Movie's storyline
+        poster_image(str): Movie's poster image
+        trailer_youtube(str): Movie's trailer via youtube
+
+    Methods:
+        show_trailer(str): opens the movie's trailer in the webbrower
+"""
+
+    def __init__(udacity, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
         udacity.title = movie_title
         udacity.storyline = movie_storyline
         udacity.poster_image_url = poster_image
         udacity.trailer_youtube_url = trailer_youtube
 
-# this opens the movie trailer
-
     def show_trailer(udacity):
+        """Opens the movie trailer in the webbrowswer
+
+        Args:
+            shower_trailer(str): opens the movie's trailer in
+            the webbrowser
+
+        Returns:
+        None.
+
+        """
         webbrowser.open(udacity.trailer_youtube_url)
-        
